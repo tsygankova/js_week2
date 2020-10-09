1dab4c4cea1dd8445bb9b0b2deb1a044ce3ffcc2
Kateryna Tsygankova <tsyg.kata@gmail.com>
2020-10-09 04:57:09 +0300

    add

---
+++ notesFunctions.js
@@ -0,0 +1,13 @@
+//functions
+
+const add = function (x,y) {
+    console.log (x);
+    console.log (y);
+    const result = x + y;
+    console.log ('x + y = ', result);
+    return result;
+};
+
+add (2, 3);
+add (6, 84);
+add (345, 12);
