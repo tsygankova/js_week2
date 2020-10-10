d2019802d683cdc1c24d8260d25637afdfe2d887
Kateryna Tsygankova <tsyg.kata@gmail.com>
2020-10-10 20:06:54 +0300

    Update notesFunctions.js

--- notesFunctions.js
+++ notesFunctions.js
@@ -8,6 +8,9 @@ const add = function (x,y) {
     return result;
 };
 
-add (2, 3);
-add (6, 84);
-add (345, 12);
+r1 = add (2, 3);
+console.log (r1);
+r2 = add (6, 84);
+console.log (r2);
+r3 = add (345, 12);
+console.log (r3);
