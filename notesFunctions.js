15fdafd909b765f63359c55b9db50e1049e0d095
Kateryna Tsygankova <tsyg.kata@gmail.com>
2020-10-12 15:37:11 +0300

    add

--- notesFunctions.js
+++ notesFunctions.js
@@ -22,3 +22,30 @@ const fn = function (value){
 console.log (fn(500));
 console.log (fn(5));
 
+// функция, которая подсчитывает сумму чисел в массиве
+
+const cart = [5, 10, 12, 6];
+
+
+
+const calculateTotalPrice = function (items){
+    let total = 0;
+    console.log ('items внутри функции', items);
+
+for (const item of items){
+    total +=item;
+}
+return total;}
+
+const cart1 = [12, 23, 5, 67]
+const c1 = calculateTotalPrice (cart1);
+console.log (c1);
+
+const cart2 = [1, 2, 3];
+const c2 = calculateTotalPrice (cart2);
+console.log (c2);
+
+const cart3 = [12, 8, 10, 23, 37];
+const c3 = calculateTotalPrice (cart3);
+console.log (c3);
+
--- script3.js
+++ script3.js
@@ -11,18 +11,30 @@
 const login = ['umrao', 'shmutza', 'tsyganek', 'instead', 'tsy_kata'];
 const loginToFind = 'umrao';
 
-for (let i = 0; i < login.length; i +=1) {
-    if (login.includes (loginToFind)){
-        console.log (`Пользователь ${loginToFind} найден`);
-    } else{
-    console.log (`Пользователь ${loginToFind} не найден`);}
-}
+// for (let i = 0; i < login.length; i +=1) {
+//     if (login.includes (loginToFind)){
+//         console.log (`Пользователь ${loginToFind} найден`);
+//     } else{
+//     console.log (`Пользователь ${loginToFind} не найден`);}
+// }
 
-for (let username of login) {
-    login.includes (loginToFind) ? console.log (`Пользователь ${loginToFind} найден`) : console.log (`Пользователь ${loginToFind} не найден`);
-}
+// for (let username of login) {
+//     login.includes (loginToFind) ? console.log (`Пользователь ${loginToFind} найден`) : console.log (`Пользователь ${loginToFind} не найден`);
+// }
 
-login.indexOf(loginToFind);
-if(login.indexOf(loginToFind) !== -1){
-    console.log (`Пользователь ${loginToFind} найден`);
-} else {console.log (`Пользователь ${loginToFind} не найден`);}
+// login.indexOf(loginToFind);
+// if(login.indexOf(loginToFind) !== -1){
+//     console.log (`Пользователь ${loginToFind} найден`);
+// } else {console.log (`Пользователь ${loginToFind} не найден`);}
+
+// написать функцию findLogin с параметрами allLogins, theLogin
+
+const findLogin = function (allLogins, theLogin){
+    for (let i = 0; i < allLogins.length; i +=1) {
+        if (allLogins.includes (theLogin)){
+            return `Пользователь ${theLogin} найден`;
+        } return `Пользователь ${theLogin} не найден`;}
+    }
+
+   const q1 = findLogin (login, loginToFind);
+   console.log (q1);
